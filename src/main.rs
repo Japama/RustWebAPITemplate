3fab43d98d004e7c58433e5334db963a48b9bb14
Juan Bautista Valero Carrasco <jbvc91@gmail.com>
2023-08-29 22:04:48 +0200

    StartUp

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod error;
+mod ctx;
+mod model;
+mod log;
+mod web;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    hc.do_get("/index.html").await?.print().await();
+
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username": "demo1",
+            "pwd": "welcome"
+        }),
+    );
+
+    req_login.await?.print().await?;
+
+    Ok(())
+}
---
+++ src/web/mod.rs
@@ -0,0 +1,4 @@
+mod mw_auth;
+mod mw_res_map;
+mod routes_login;
+mod routes_static;
